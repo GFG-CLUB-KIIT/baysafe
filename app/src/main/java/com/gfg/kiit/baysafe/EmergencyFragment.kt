bff718efa43c0ad083aadf8421c4822d332ba2e3
abhisek30 <abhisekcsce@gmail.com>
2021-10-30 18:57:42 +0530

    Project Structure

---
+++ app/src/main/java/com/gfg/kiit/baysafe/EmergencyFragment.kt
@@ -0,0 +1,19 @@
+package com.gfg.kiit.baysafe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EmergencyFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_emergency, container, false)
+    }
+}
--- app/src/main/java/com/gfg/kiit/baysafe/MainActivity.kt
+++ app/src/main/java/com/gfg/kiit/baysafe/MainActivity.kt
@@ -2,10 +2,21 @@ package com.gfg.kiit.baysafe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.gfg.kiit.baysafe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bottomNavigationView = binding.bottomNavigationView
+        val navController = findNavController(R.id.navHostFragment)
+        bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView.itemIconTintList = null
     }
 }
